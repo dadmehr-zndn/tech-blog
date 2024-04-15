e7bc656fa76adbcfdfa3702fde2314ae59f323dc
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-04-15 19:13:51 +0330

    string constants added

---
+++ lib/constants.dart
@@ -0,0 +1,115 @@
+class Strings {
+  // main-1
+  static const String changeProfilePhoto = 'ویرایش عکس پروفایل';
+  static const String myFavouriteArticles = 'مقالات مورد علاقه من';
+  static const String myFavouritePodcasts = 'پادکست های مورد علاقه من';
+  static const String signOutOfTheAccount = 'خروج از حساب کاربری';
+
+  // mataleb - 4
+  static const String favouriteArticles = 'مقالات مورد علاقه';
+
+  // mataleb - 5
+  static const String favouritePodcasts = 'پادکست های مورد علاقه';
+
+  // main
+  static const String viewTheHottestArticles = 'مشاهده داغ ترین نوشته ها ';
+  static const String viewTheHottestPodcasts = 'مشاهده داغ ترین پادکست ها';
+
+  // mataleb
+  static const String listOfArticles = 'لیست مقاله ها';
+
+  //matelb-1
+  static const String listOfPodcasts = 'لیست پادکست ها';
+
+  // blog_content
+  static const String relatedArticles = 'نوشته های مرتبط';
+
+  // Drawer Menu
+  static const String userProfile = 'پروفایل کاربری';
+  static const String aboutTechBlog = 'درباره تک بلاگ';
+  static const String sharingTechBlog = 'اشتراک گذاری تک بلاگ';
+  static const String techBlogOnGithub = 'تک بلاگ در گیت هاب';
+
+  // main-2
+  static const String shareYourKnowledgeWithOthers =
+      'دونسته هات رو با بقیه به اشتراک بذار ...';
+  static const String thinkBeingHereMeans =
+      '''فکر کن !!  اینجا بودنت به این معناست که یک گیک تکنولوژی هستی
+دونسته هات رو با  جامعه‌ی گیک های فارسی زبان به اشتراک بذار..''';
+  static const String manageArticles = 'مدیریت مقاله ها';
+  static const String managePodcasts = 'مدیریت پادکست ها';
+
+  // نوشتن مقاله - 1
+  static const String welcomeToTechBlog = '''به تک‌بلاگ خوش اومدی
+
+برای ارسال مطلب و پادکست باید حتما 
+ثبت نام کنی ''';
+  static const String buttonLetsGo = 'بزن بریم';
+  static const String plsEnterYourEmail = 'لطفا ایمیلت رو وارد کن';
+  static const String buttonContinue = 'ادامه';
+  static const String techBlogEmail = 'techblog@gmail.com';
+  static const String enterTheActivationCode = 'کد فعال سازی رو وارد کن ';
+  static const String textFieldStart = '******';
+  static const String congratsEmailSuccess =
+      '''تبریک میگم ، ایمیل با موفقیت تایید شد
+لطفا اطلاعات ثبت نام رو کامل کن''';
+  static const String chooseTheCategoryLike =
+      'دسته بندی هایی که دوست داری رو انتخاب کن';
+
+  // مدیریت مقاله ها - 1
+  static const String noArticlesAvaiable =
+      '''هنوز هیچ مقاله ای به جامعه گیک های فارسی 
+اضافه نکردی !!!''';
+  static const String buttonWriteArticle = 'بریم برای نوشتن یه مقاله باحال';
+
+  //مدیریرت مقاله ها - 2
+  static const String waiting = 'در انتظار';
+  static const String accepted = 'تایید شده';
+  static const String draft = 'پیش نویس';
+
+  // پیش نمایش
+  static const String choosePicture = 'انتخاب تصویر';
+  static const String editArticleTitle = 'ویرایش عنوان مقاله';
+  static const String hereArticleTitle =
+      'اینجا عنوان مقاله قرار میگیره ، یه عنوان جذاب انتخاب کن';
+  static const String editMainArticleText = 'ویرایش متن اصلی مقاله';
+  static const String iAmMainArticleBody =
+      'من متن و بدنه اصلی مقاله هستم ، اگه میخوای من رو ویرایش کنی و یه مقاله جذاب بنویسی ، نوشته آبی رنگ بالا که نوشته "ویرایش متن اصلی مقاله" رو با انگشتت لمس کن تا وارد ویرایشگر بشی';
+  static const String chooseCategory = 'انتخاب دسته بندی ';
+  static const String buttonDone = 'انتخاب دسته بندی ';
+
+  // نوشتن متن اصلی
+  static const String writeAttractiveArticle = 'یک مقاله جذاب بنویس';
+  static const String buttonPreview = 'پیش نمایش';
+  static const String completeLater = 'بعدا کامل میکنم';
+
+  // ویرایش عنوان مقاله
+  static const String chooseTitle = 'لطفا یه عنوان پر معنا و کوتاه انتخاب کن';
+  static const String enterTitle = 'عنوان را وارد کنید';
+  static const String buttonLater = 'بعدا';
+  static const String buttonSubmit = 'تایید';
+
+  // پیش نمایش - 2
+  static const String clickTagToAdd =
+      'روی دسته بندی مرتبط ضربه بزن تا به مقاله اضافه بشه';
+
+  // mataleb - 2
+  static const String addNewPodcast = 'اضافه کردن یه پادکست جدید';
+
+  // mataleb - 3
+  static const String noPodcastAvailabe =
+      '''هنوز هیچ پادکستی به جامعه گیک های فارسی 
+اضافه نکردی !!!''';
+
+// اضافه کرن پادکست جدید
+  static const String editPodcastTitle = 'ویرایش عنوان پادکست';
+  static const String podcastName = 'پادکست : اسم پادکست اینجا قرار می‌گیره';
+  static const String addFile = 'اضافه کردن فایل';
+
+  // اضافه کردن پادکست جدید – 1
+  static const String enterPodcastTimeAndFile =
+      'لطفا زمان و فایل قسمت جدید پادکست رو وارد کن';
+  static const String chooseAudioFile = 'انتخاب فایل صوتی';
+  static const String seconds = 'ثانیه';
+  static const String minutes = 'دقیقه';
+}
