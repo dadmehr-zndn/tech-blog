336470e1e14dea7434dffa179ef7d142a16df4bf
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-04-26 12:28:06 +0330

    top visited podcasts and articles models added

---
+++ lib/models/top_articles_model.dart
@@ -0,0 +1,35 @@
+class TopArticlesModel {
+  TopArticlesModel({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.catId,
+    required this.catName,
+    required this.author,
+    required this.view,
+    required this.status,
+    required this.createdAt,
+  });
+
+  String? id;
+  String? title;
+  String? image;
+  String? catId;
+  String? catName;
+  String? author;
+  String? view;
+  String? status;
+  String? createdAt;
+
+  TopArticlesModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    id = json['title'];
+    id = json['image'];
+    id = json['catId'];
+    id = json['catName'];
+    id = json['author'];
+    id = json['view'];
+    id = json['status'];
+    id = json['createdAt'];
+  }
+}
---
+++ lib/models/top_podcasts_model.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class TopPodcastsModel {
+  TopPodcastsModel({
+    required this.id,
+    required this.title,
+    required this.poster,
+    required this.catName,
+    required this.author,
+    required this.view,
+    required this.status,
+    required this.createdAt,
+  });
+
+  String? id;
+  String? title;
+  String? poster;
+  String? catName;
+  String? author;
+  String? view;
+  String? status;
+  String? createdAt;
+
+  TopPodcastsModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    id = json['title'];
+    id = json['poster'];
+    id = json['catName'];
+    id = json['author'];
+    id = json['view'];
+    id = json['status'];
+    id = json['createdAt'];
+  }
+}
